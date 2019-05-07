19d566c9c88e431b4f55f18cd845a5be20d52c8d
sunfx <123074528@qq.com>
2019-05-07 14:39:25 +0800

    更新

--- auth-cas/src/main/java/com/lingchaomin/auth/cas/authentication/YBQueryDatabaseAuthenticationHandler.java
+++ auth-cas/src/main/java/com/lingchaomin/auth/cas/authentication/YBQueryDatabaseAuthenticationHandler.java
@@ -25,7 +25,8 @@ import javax.validation.constraints.NotNull;
  */
 public class YBQueryDatabaseAuthenticationHandler extends AbstractJdbcUsernamePasswordAuthenticationHandler {
 
-    private static final Logger log= LoggerFactory.getLogger(YBQueryDatabaseAuthenticationHandler.class);
+    private static final Logger log = LoggerFactory.getLogger(YBQueryDatabaseAuthenticationHandler.class);
+
     @NotNull
     private String passwordSql;
 
@@ -36,29 +37,30 @@ public class YBQueryDatabaseAuthenticationHandler extends AbstractJdbcUsernamePa
     }
 
     protected final HandlerResult authenticateUsernamePasswordInternal(UsernamePasswordCredential credential) throws GeneralSecurityException, PreventedException {
+
         String username = credential.getUsername();
 
-
         try {
-            String e =this.getJdbcTemplate().queryForObject(this.passwordSql, String.class, new Object[]{username});
-            String salt=this.getJdbcTemplate().queryForObject(this.saltSql, String.class, new Object[]{username});
-
-            String encryptedPassword = this.getPasswordEncoder().encode(credential.getPassword()+salt);
-            log.info("密码验证:{},{}",encryptedPassword,e);
-            if(!e.equals(encryptedPassword)) {
+            //原始密码
+            String entry_password = this.getJdbcTemplate().queryForObject(this.passwordSql, String.class, new Object[]{username});
+            //盐值
+            String salt = this.getJdbcTemplate().queryForObject(this.saltSql, String.class, new Object[]{username});
+            //加密后密码
+            String encryptedPassword = this.getPasswordEncoder().encode(credential.getPassword() + salt);
+            log.info("密码验证:{},{}", encryptedPassword, entry_password);
+            if (!entry_password.equals(encryptedPassword)) {
                 throw new FailedLoginException("Password does not match value on record.");
             }
         } catch (IncorrectResultSizeDataAccessException var5) {
-            if(var5.getActualSize() == 0) {
+            if (var5.getActualSize() == 0) {
                 throw new AccountNotFoundException(username + " not found with SQL query");
             }
-
             throw new FailedLoginException("Multiple records found for " + username);
         } catch (DataAccessException var6) {
             throw new PreventedException("SQL exception while executing query for " + username, var6);
         }
 
-        return this.createHandlerResult(credential, new SimplePrincipal(username), (List)null);
+        return this.createHandlerResult(credential, new SimplePrincipal(username), (List) null);
     }
 
 
